811e9629082d3618ad0ad1ea79e5b0f55e66650b
Joe Thorley <joe@poissonconsulting.ca>
2025-05-15 04:39:13 +0800

    yanagihara workup

--- R/yanagihara-24-acute.R
+++ R/yanagihara-24-acute.R
@@ -1,6 +1,6 @@
 #' Acute Species Sensitivity Data as compiled by Yanagihara et al. (2024)
 #' 
-#' Acute species sensitivity data from Envirotox as worked up by Yanagihara et al. (2024).
+#' Acute species sensitivity data from Envirotox (Connors et al. 2019) as worked up by Yanagihara et al. (2024).
 #' It is important to note that Yanagihara et al. (2024) 
 #' exclude datasets with a bimodality coefficient (BC) > 0.555.
 #' 
@@ -14,7 +14,10 @@
 #'\item{Group}{Acute (chr).}
 #'\item{Original_CAS}{Original CAS (chr).} 
 #' }
+#' @references
+#' Yanagihara, M., Hiki, K., and Iwasaki, Y. 2024. Which distribution to choose for deriving a species sensitivity distribution? Implications from analysis of acute and chronic ecotoxicity data. Ecotoxicology and environmental safety 278: 116379. doi:10.1016/j.ecoenv.2024.116379.
 #' 
+#' Connors, K.A., Beasley, A., Barron, M.G., Belanger, S.E., Bonnell, M., Brill, J.L., de Zwart, D., Kienzler, A., Krailler, J., Otter, R., Phillips, J.L., and Embry, M.R. 2019. Creation of a Curated Aquatic Toxicology Database: EnviroTox. Enviro Toxic and Chemistry 38(5): 1062–1073. doi:10.1002/etc.4382.
 #' @name yanagihara_24_acute
 #' @docType data
 #' @keywords datasets
