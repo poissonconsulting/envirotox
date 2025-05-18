7998e32cf0b7d164c641f5d91a0c87a66b564372
Joe Thorley <joe@poissonconsulting.ca>
2025-05-18 11:18:16 +0800

    update documentation acute and chronic

--- R/envirotox-chronic.R
+++ R/envirotox-chronic.R
@@ -1,28 +1,18 @@
 #' Chronic Species Sensitivity Datasets
 #' 
-#' Envirotox (Connors et al. 2019) chronic species sensitivity datasets that 
-#' include those of Yanagihara et al. (2024).
-#' As far as we are aware the only difference is that the current datasets
-#' also include those with a bimodality coefficient (BC) > 0.555 as well as
-#' those that fail to converge with the BurrIII distribution.
-#' 
-#' The columns are as follows:
-#' 
-#' \describe{ 
-#'\item{Chemical}{The chemical name (chr).}
-#'\item{Conc}{The chemical concentration in micrograms per litre (dbl).}
-#'\item{Species}{The species name (chr).}
-#'\item{Group}{Acute (chr).}
-#'\item{Original_CAS}{Original CAS (chr).} 
-#'\item{BC}{Bimodality Coefficient for the Original CAS (chr).} 
+#' @format ## `envirotox_chronic`
+#' A data frame with 1,721 rows and 6 columns:
+#' \describe{
+#'  \item{Chemical}{Chemical name (chr)}
+#'  \item{Conc}{Concentration of chemical in micrograms per litre (dbl).}
+#'  \item{Species}{Species name (chr).}
+#'  \item{Group}{Taxonomic group of species (chr).}
+#'  \item{Yanagihara24}{Whether the dataset fits the criteria of Yanagihara et al. (2024) (flag).}
+#'  \item{Bimodality}{The bimodality coefficient (dbl).}
 #' }
+#' @source <https://envirotoxdatabase.org/>
 #' @references
 #' Yanagihara, M., Hiki, K., and Iwasaki, Y. 2024. Which distribution to choose for deriving a species sensitivity distribution? Implications from analysis of acute and chronic ecotoxicity data. Ecotoxicology and environmental safety 278: 116379. doi:10.1016/j.ecoenv.2024.116379.
-#' 
-#' Connors, K.A., Beasley, A., Barron, M.G., Belanger, S.E., Bonnell, M., Brill, J.L., de Zwart, D., Kienzler, A., Krailler, J., Otter, R., Phillips, J.L., and Embry, M.R. 2019. Creation of a Curated Aquatic Toxicology Database: EnviroTox. Enviro Toxic and Chemistry 38(5): 1062–1073. doi:10.1002/etc.4382.
-#' @name envirotox_chronic
-#' @docType data
-#' @keywords datasets
 #' @examples
 #' 
 #' head(envirotox_chronic)
