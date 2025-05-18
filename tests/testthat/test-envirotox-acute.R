e33fc7b4b311b30fa9cc7b251ad5b99e1f921079
Joe Thorley <joe@poissonconsulting.ca>
2025-05-18 09:55:05 +0800

    update snaps

--- tests/testthat/test-envirotox-acute.R
+++ tests/testthat/test-envirotox-acute.R
@@ -1,5 +1,5 @@
 test_that("envirotox-acute", {
   chk::check_key(envirotox::envirotox_acute, c("Chemical", "Species"))
-  
+  expect_identical(nrow(envirotox_acute), 14949L)
   expect_snapshot_data(envirotox_acute, "envirotox_acute")
 })
--- tests/testthat/test-envirotox-chronic.R
+++ tests/testthat/test-envirotox-chronic.R
@@ -1,5 +1,5 @@
 test_that("envirotox-chronic", {
   chk::check_key(envirotox::envirotox_chronic, c("Chemical", "Species"))
-  
+  expect_identical(nrow(envirotox_chronic), 1721L)
   expect_snapshot_data(envirotox_chronic, "envirotox_chronic")
 })
